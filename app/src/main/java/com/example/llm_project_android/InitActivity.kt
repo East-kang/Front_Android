8dee67eda53091f823ccb7c4eaad7418f89fdd0f
East-kang <donghyun2047@naver.com>
2025-06-15 16:31:10 +0900

    Created InitActivity.kt and init_view.xml

---
+++ app/src/main/java/com/example/llm_project_android/InitActivity.kt
@@ -0,0 +1,35 @@
+package com.example.llm_project_android
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class InitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.init_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btn_login = findViewById<Button>(R.id.login_Button)         // 로그인 버튼 선언
+        val btn_sign_up = findViewById<Button>(R.id.sign_up_Button)     // 회원가입 버튼 선언
+
+        btn_login.setOnClickListener {                                      // 로그인 버튼 클릭 메서드
+            val intent1 = Intent(this, LoginActivity::class.java)
+            startActivity(intent1)
+        }
+
+        btn_sign_up.setOnClickListener {                                      // 회원가입 버튼 클릭 메서드
+            val intent2 = Intent(this, SignUpActivity1::class.java)
+            startActivity(intent2)
+        }
+    }
+}
--- app/src/main/java/com/example/llm_project_android/LoginActivity.kt
+++ app/src/main/java/com/example/llm_project_android/LoginActivity.kt
@@ -1,6 +1,9 @@
 package com.example.llm_project_android
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,17 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val id_text = findViewById<EditText>(R.id.id_editText)          // 아이디 입력창
+        val pw_text = findViewById<EditText>(R.id.password_editText)    // 비밀번호 입력창
+        val btn_login = findViewById<Button>(R.id.login_Button)         // 로그인 버튼
+        val signUp_text = findViewById<TextView>(R.id.signUpText)       // 회원가입 텍스트
+        var check_id : Boolean = false                                      // 아이디 존재 여부
+        var check_pw : Boolean = false                                      // 비밀번호 존재 여부
+
+        btn_login.setOnClickListener {
+
         }
 
     }
