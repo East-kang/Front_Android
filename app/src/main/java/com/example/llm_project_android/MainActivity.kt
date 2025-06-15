87c12a3387d9221088b50f3848d0ce001fbca8de
East-kang <donghyun2047@naver.com>
2025-06-15 16:16:28 +0900

    MainActivity.kt Implementation Completed

--- app/src/main/java/com/example/llm_project_android/MainActivity.kt
+++ app/src/main/java/com/example/llm_project_android/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.llm_project_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,7 +13,14 @@ class MainActivity : AppCompatActivity() {
         val btn_login = findViewById<Button>(R.id.login_Button)         // 로그인 버튼 선언
         val btn_sign_up = findViewById<Button>(R.id.sign_up_Button)     // 회원가입 버튼 선언
 
+        btn_login.setOnClickListener {                                      // 로그인 버튼 클릭 메서드
+            val intent1 = Intent(this, LoginActivity::class.java)
+            startActivity(intent1)
+        }
 
-
+        btn_sign_up.setOnClickListener {                                      // 회원가입 버튼 클릭 메서드
+            val intent2 = Intent(this, SignUpActivity1::class.java)
+            startActivity(intent2)
+        }
     }
 }
