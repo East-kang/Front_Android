f2f9a02d0cc99e116c614e59f2eb091a170da85a
East-kang <donghyun2047@naver.com>
2025-06-17 17:16:36 +0900

    Add Function_Visible_setOnClickListener.kt

--- app/src/main/java/com/example/llm_project_android/SignUpActivity1.kt
+++ app/src/main/java/com/example/llm_project_android/SignUpActivity1.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.graphics.toColorInt
 
 class SignUpActivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,10 +35,12 @@ class SignUpActivity1 : AppCompatActivity() {
         val pw_check_text = findViewById<TextView>(R.id.pw_check_text)
         val email_check = findViewById<TextView>(R.id.email_check)
 
-        val id_test: String = "qwer1234"
-        val pw_test: String = "qwer1234"
-        var check_id: Boolean = false
-        var check_pw: Boolean = false
+        val id_test: String = "qwer1234"        // 테스트 아이디
+        val pw_test: String = "qwer1234"        // 테스트 비밀번호
+        var check_id: Boolean = false           // 아이디 중복 여부
+        var check_pw: Boolean = false           // 비밀번호 일치 여부
+        var pw_visible: Boolean = false         // 비밀번호 시각화 여부 (true: 시각화, false: 비시각화)
+        var pw_check_visible: Boolean = false   // 비밀번호 확인 시각화 여부 (true: 시각화, false: 비시각화)
 
         // 아이디 생성
         create_id(id_text, id_rule, id_test, btn_idCheck)
@@ -45,56 +48,62 @@ class SignUpActivity1 : AppCompatActivity() {
         // 비밀번호 생성
         create_pw()
 
-        // 중복 확인 버튼 클릭 이벤트
-//        btn_idCheck.setOnClickListener {
-//            // 기존 아이디 존재
-//            if (id_text.text.toString() == id_test) {
-//                id_rule.setText("이미 존재하는 아이디입니다")
-//                id_rule.setTextColor(0xFF0000)
-//            } else {
-//                id_rule.setText("사용 가능한 아이디입니다")
-//                id_rule.setTextColor(0x1F70CC)
-//            }
-//        }
+        // 비밀번호 시각화 버튼 클릭 이벤트
+        btn_eye.setOnClickListener {
+            var pos = pw_text.selectionStart            // 커서 위치 저장
+            pw_visible = togglePasswordVisibility(pw_text, pw_visible, btn_eye)      // 비밀번호 시각화
+            pw_text.post{ pw_text.setSelection(pos) }   // 커서 위치 복원
+        }
+
+        btn_eye_check.setOnClickListener {
+            var pos = pw_text.selectionStart            // 커서 위치 저장
+            pw_check_visible = togglePasswordVisibility(pw_check, pw_check_visible, btn_eye_check)      // 비밀번호 시각화
+            pw_check.post{ pw_text.setSelection(pos) }   // 커서 위치 복원
+        }
     }
 
     // 아이디 생성 기능 함수
-    fun create_id(input: EditText, id_rule: TextView, id_test: String, btn_idCheck: Button) {
+    fun create_id(input: EditText, rule: TextView, test: String, idCheck: Button) {
         val id_Pattern = Regex("^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z[0-9]]{6,12}$")    // 영문, 숫자 (6-12자리)
 
         // 중복 확인 버튼 클릭 이벤트
-        btn_idCheck.setOnClickListener {
+        idCheck.setOnClickListener {
             // 기존 아이디 존재
-            if (input.text.toString() == id_test) {
-                id_rule.setText("이미 존재하는 아이디입니다")
-                id_rule.setTextColor(0xFF0000)
+            if (input.text.toString() == test) {
+                rule.setText("이미 존재하는 아이디입니다")
+                rule.setTextColor("#FF0000".toColorInt())
             } else {
-                id_rule.setText("사용 가능한 아이디입니다")
-                id_rule.setTextColor(0x1F70CC)
+                rule.setText("사용 가능한 아이디입니다")
+                rule.setTextColor("#1F70CC".toColorInt())
             }
         }
 
         // id_textView 실시간 감지 이벤트 (정규식에 대한 버튼 활성화 여부)
         input.addTextChangedListener(
             createFlexibleTextWatcher(
-                targetTextView = id_rule,
+                targetTextView = rule,
                 updateText = {"6~12자의 영문, 숫자를 사용하세요"},
                 updateTextColor = {android.graphics.Color.parseColor("#1F70CC")},
                 validateInput = {input -> id_Pattern.matches(input)},
-                onValidStateChanged = {isValid -> btn_idCheck.isEnabled = isValid
-                    btn_idCheck.setBackgroundResource(
-                        if (isValid)
-                            R.drawable.login_button // 사용 가능 상태 색
-                        else
-                            R.drawable.id_check_button  // 비활성 상태 색
+                onValidStateChanged = {isValid -> idCheck.isEnabled = isValid
+                    idCheck.setBackgroundResource(
+                        if (isValid)    { R.drawable.login_button }     // 사용 가능 상태
+                        else            { R.drawable.id_check_button }  // 비활성 상태
                     )}))
-
-
     }
 
     // 비밀번호 생성 기능 함수
-    fun create_pw() {
+    fun create_pw(input: EditText, rule: TextView, test: String, pwCheck: Button) {
         val pw_Pattern = Regex("^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z[0-9]]{6,12}$")    // 영문, 숫자 (8-20자리)
 
+    }
+
+    fun pw_eye_visibility (btn: ImageButton, text: EditText, get_visible: () -> Boolean, set_visible: (Boolean) -> Unit) {
+        btn.setOnClickListener {
+            val pos = text.selectionStart           // 커서 위치 저장
+            val newVisible = (togglePasswordVisibility(text, get_visible(), btn))      // 비밀번호 시각화
+            set_visible(newVisible)                 // Boolean 값 업데이트
+            text.post{ text.setSelection(pos) }     // 커서 위치 복원
+        }
     }
 }
