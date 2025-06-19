8edc636b19c6bea6cebcd5c2541b964df2fb6147
East-kang <donghyun2047@naver.com>
2025-06-19 19:56:17 +0900

    X

--- app/src/main/java/com/example/llm_project_android/InitActivity.kt
+++ app/src/main/java/com/example/llm_project_android/InitActivity.kt
@@ -3,6 +3,7 @@ package com.example.llm_project_android
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
