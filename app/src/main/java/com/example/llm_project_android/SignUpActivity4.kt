7e7b3f571771494a36b8b930b8e5ef5fedc1eb0a
East-kang <donghyun2047@naver.com>
2025-06-27 16:11:44 +0900

    Reconfiguring View Structures

--- app/src/main/java/com/example/llm_project_android/SignUpActivity4.kt
+++ app/src/main/java/com/example/llm_project_android/SignUpActivity4.kt
@@ -1,14 +1,23 @@
 package com.example.llm_project_android
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.chip.ChipGroup
 
 class SignUpActivity4 : AppCompatActivity() {
 
-    private lateinit var insurance_Group
+    private lateinit var insurance: RadioGroup
+    private lateinit var insurance_Y: RadioButton
+    private lateinit var insurance_N: RadioButton
+    private lateinit var search_insurance: EditText
+    private lateinit var tag: ChipGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
