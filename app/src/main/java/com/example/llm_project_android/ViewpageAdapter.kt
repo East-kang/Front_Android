69bb02c293e9c369189b1e7228fe9f429db4477d
East-kang <donghyun2047@naver.com>
2025-07-01 15:37:51 +0900

    배너 어댑터 생성

---
+++ app/src/main/java/com/example/llm_project_android/ViewpageAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.llm_project_android
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.llm_project_android.databinding.ItemBannerBinding
+
+class BannerAdapter(private val images : List<Int>) :
+    RecyclerView.Adapter<BannerAdapter.BannerViewHolder>() {
+
+    inner class BannerViewHolder(private val binding : ItemBannerBinding) :
+        RecyclerView.ViewHolder(binding.root){
+        fun bind(item : Int) {
+            binding.ivBanner.setImageResource(item)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BannerViewHolder {
+        val binding = ItemBannerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BannerViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BannerViewHolder, position: Int) {
+        holder.bind(images[position])
+    }
+
+    override fun getItemCount(): Int = images.size
+}
