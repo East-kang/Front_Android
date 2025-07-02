4a4d56e41b37b6a4f7bd38702e917bed5114c894
East-kang <donghyun2047@naver.com>
2025-07-02 16:21:41 +0900

    메뉴 열림 기능 구현 완료

--- app/src/main/java/com/example/llm_project_android/MainViewActivity.kt
+++ app/src/main/java/com/example/llm_project_android/MainViewActivity.kt
@@ -1,6 +1,7 @@
 package com.example.llm_project_android
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,16 +10,21 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.llm_project_android.databinding.MainViewBinding
 import android.view.View
 import android.widget.ImageButton
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
+import com.google.android.material.navigation.NavigationView
 
 class MainViewActivity : AppCompatActivity() {
     private lateinit var binding: MainViewBinding
     private val sliderHandler = android.os.Handler(android.os.Looper.getMainLooper())
     private lateinit var sliderRunnable: Runnable
 
+    private lateinit var drawerLayout: DrawerLayout
     private lateinit var btn_search: ImageButton
-    private lateinit var btn_menu: ImageButton
+    private lateinit var btn_menu_black: ImageButton
+    private lateinit var menuView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +33,18 @@ class MainViewActivity : AppCompatActivity() {
         // 바인딩 초기화
         binding = MainViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
         btn_search = findViewById<ImageButton>(R.id.search_icon)
-        btn_menu = findViewById<ImageButton>(R.id.menu_icon)
+        btn_menu_black = findViewById<ImageButton>(R.id.menu_icon_black)
+        menuView = findViewById<NavigationView>(R.id.navigationView)
 
+        // 배너 아이템 리스트
         val bannerList = listOf(
             R.drawable.image_birth_icon,
             R.drawable.sample_image,
@@ -46,6 +55,8 @@ class MainViewActivity : AppCompatActivity() {
         setupViewPager(bannerList)
         startAutoScroll(bannerList)
 
+        // 메뉴 클릭 이벤트
+        menu_control()
     }
 
     // 배너 양 옆 이미지 노출 함수
@@ -99,6 +110,16 @@ class MainViewActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         sliderHandler.removeCallbacks(sliderRunnable)
+    }
+
+    // 메뉴 기능
+    private fun menu_control() {
+        btn_menu_black.setOnClickListener {
+            if (drawerLayout.isDrawerOpen(GravityCompat.END))
+                drawerLayout.closeDrawer(GravityCompat.END)
+            else
+                drawerLayout.openDrawer(GravityCompat.END)
+        }
     }
 
 }
