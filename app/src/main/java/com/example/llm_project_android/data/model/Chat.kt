034b434e961c9a3074fff15704b6d17f031a3166
East-kang <donghyun2047@naver.com>
2025-07-12 13:55:43 +0900

    ChatAdapter 구현

--- app/src/main/java/com/example/llm_project_android/data/model/Chat.kt
+++ app/src/main/java/com/example/llm_project_android/data/model/Chat.kt
@@ -1,13 +1,9 @@
 package com.example.llm_project_android.data.model
 
-data class Chat (
-    var messageID: Int,     // 메시지 ID
-    var role: String,       // 역할
-    var content: List<MessageData>     // 내용
-)
-
-data class MessageData (
-    val content: String,
-    val createdAt: Long,
-    val from: Int
+data class Chat(
+    val content: String,    // 메시지 내용
+    val isUser: Boolean,    // 유저 판별
+    val timestamp: Long,    // 메시지 생성 시간
+    val suggestions: List<String>? = null,  // 빠른 질문 버튼 목록
+    val showTime: Boolean = false // 이 메시지 밑에 시간을 표시할지 여부
 )
