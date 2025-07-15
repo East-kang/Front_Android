4a7611e0b1f2040d899d60fd75ee956e86f31aea
East-kang <donghyun2047@naver.com>
2025-07-15 17:57:24 +0900

    DB 저장용 Entity 파일 생성

--- app/src/main/java/com/example/llm_project_android/data/local/entity.kt
+++ app/src/main/java/com/example/llm_project_android/data/local/entity.kt
@@ -1,7 +1,9 @@
 package com.example.llm_project_android.data.local
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+// DB 저장용 Entity
 @Entity(tableName = "chat_history")
 data class ChatEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
