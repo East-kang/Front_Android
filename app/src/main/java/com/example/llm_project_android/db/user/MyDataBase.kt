4f2aedb5daa692772ab8c5abce28abbbe8870bf7
East-kang <donghyun2047@naver.com>
2025-08-01 18:37:26 +0900

    -

--- app/src/main/java/com/example/llm_project_android/db/user/MyDataBase.kt
+++ app/src/main/java/com/example/llm_project_android/db/user/MyDataBase.kt
@@ -7,12 +7,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-<<<<<<<< HEAD:app/src/main/java/com/example/llm_project_android/db/Users/MyDataBase.kt
-import com.example.llm_project_android.db.Converter.Converters
 import com.example.llm_project_android.db.Users.User
-========
 import com.example.llm_project_android.db.converter.Converters
->>>>>>>> c21d2fdee798c3bd8221462e4bf9e3886670ad93:app/src/main/java/com/example/llm_project_android/db/user/MyDataBase.kt
 
 //Room database의 기본틀
 @Database(entities = [User::class], version = 1, exportSchema = false)
