42b87a41e4581043c7fb370ee9168c249dfacb44
East-kang <donghyun2047@naver.com>
2025-08-13 17:57:29 +0900

    비교하기 뷰 생성

---
+++ app/src/main/java/com/example/llm_project_android/page/c_product/CompareViewActivity.kt
@@ -0,0 +1,21 @@
+package com.example.llm_project_android.page.c_product
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.llm_project_android.R
+
+class CompareViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.c_page_compare_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
